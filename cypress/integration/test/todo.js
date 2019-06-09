776f2e9ee8f654bbf42e866f6d8880317548e128
JianJiaCheng <576400783@qq.com>
2019-06-09 10:31:22 +0800

    cypress

---
+++ cypress/integration/test/todo.js
@@ -0,0 +1,32 @@
+describe("todolist test",() => {
+    it("add a new todo task", () => {
+        cy.visit("http://localhost:8080");
+        cy.wait(1000);
+        cy.get('#app > div > form > input').type('麦当劳');
+        cy.get('#app > div > form > button').click();
+        cy.wait(1000);
+
+
+    });
+    it("add a new todo task", () => {
+        cy.get('#app > div > form > input').type('肯德基');
+        cy.get('#app > div > form > button').click();
+        cy.wait(1000);
+    });
+    it("edit first todo task", () => {
+        cy.get('#app > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > button:nth-child(1)').click()
+        cy.get('#app > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > form > input').clear();
+        cy.get('#app > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > form > input').type('德克士');
+        cy.wait(1000);
+        cy.get("#app > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > button:nth-child(1)").click()
+        cy.wait(1000);
+    });
+    it("delete first todo task", () => {
+        cy.get('#app > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > button.delete-btn').click()
+        cy.wait(1000);
+    });
+
+    after(() => {
+        cy.clearCookies();
+    });
+});
